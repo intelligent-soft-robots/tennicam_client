03f4e969b125cba792461096bc4e06e9126ccdf8
Vincent Berenz <vberenz@tuebingen.mpg.de>
2022-05-30 13:32:12 +0200

    started robot log dev

--- bin/tennicam_client_display.py
+++ bin/tennicam_client_display.py
@@ -6,6 +6,8 @@ as published by tennicam.
 Required for this executable:
 - in a first terminal: 'tennicam_client'
 - in second terminal :'pam_mujoco tennicam_client_display'
+Optionally, if you also wish the real robot to be mirrored:
+set the real robot segment_id using the dialog.
 """
 
 import o80
@@ -39,7 +41,23 @@ def _get_handle():
     return handle
 
 
-def run():
+def mirror_ball(frontend_from, frontend_to,duration_ms):
+    # getting information from tennicam
+    ball_zmq = frontend_from.latest()
+    position = ball_zmq.get_position()
+    velocity = ball_zmq.get_velocity()
+    # sending related command to mujoco ball
+    frontend_to.add_command(position, velocity, duration_ms, o80.Mode.OVERWRITE)
+    frontend_to.pulse()
+    
+
+def run(
+        tennicam_client_segment_id: str,
+        real_robot_segment_id: typing.Optional[str],
+        robot_type: typing.Optional[pam_mujoco.RobotType],
+        robot_position: typing.Optional[typing.Tuple[float,float,float]],
+        robot_orientation: typing.Optional[str]
+):
 
     global TENNICAM_CLIENT_DEFAULT_SEGMENT_ID
 
@@ -60,18 +78,61 @@ def run():
     signal_handler.init()  # for detecting ctrl+c
     try:
         while not signal_handler.has_received_sigint():
-            # getting information from tennicam
-            ball_zmq = frontend.latest()
-            position = ball_zmq.get_position()
-            velocity = ball_zmq.get_velocity()
-            # sending related command to mujoco ball
-            ball.add_command(position, velocity, duration_ms, o80.Mode.OVERWRITE)
-            ball.pulse()
+            mirror_ball(frontend,ball,duration_ms)
             # imposing frequency
             frequency_manager.wait()
     except (KeyboardInterrupt, SystemExit):
         return
 
 
+def _configure() -> BrightArgs:
+    """
+    Configuration dialog
+    """
+
+    global TENNICAM_CLIENT_DEFAULT_SEGMENT_ID
+    config = BrightArgs("tennicam display")
+    config.add_option(
+        "segment_id",
+        TENNICAM_CLIENT_DEFAULT_SEGMENT_ID,
+        "segment_id of the o80 backend",
+        str,
+    )
+    config.add_option(
+        "robot_segment_id",
+        None,
+        "segment_id of the real robot (optional)",
+        str,
+    )
+    config.add_option(
+        "robot_type",
+        str(pam_mujoco.RobotType.PAMY2),
+        "type of the robot, pamy1 ({}) or pamy2 ({}) (used only if robot_segment_id not None)".format(
+            str(pam_mujoco.RobotType.PAMY1), str(pam_mujoco.RobotType.PAMY2)
+        ),
+        str,
+    )
+    config.add_option(
+        "robot_position",
+        "[0.,0.,1.21]",
+        "3d position of the robot (as string) (used only if robot_segment_id not None)"
+        str,
+    )
+    config.add_option(
+        "robot_orientation",
+        "0.0 0.0 0.0",
+        "orientation of the robot (as string) (used only if robot_segment_id not None)"
+        str,
+    )
+    change_all = False
+    config.dialog(change_all, sys.argv[1:])
+    print()
+    return config
+
+def run():
+    config = _configure()
+
+
+    
 if __name__ == "__main__":
     run()
--- bin/tennicam_client_logger.py
+++ bin/tennicam_client_logger.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python3
 
 """
-Dump the output of tennicam_client into a file,
-with format for each line:
+Dump the output of tennicam_client into a file.
+The first line is reserved for meta-data, which is by default empty.
+Then, format for each line:
  repr([ball_id,time_stamp,position,velocity])
 with:
 - ball_id : int
 - time_stamp: int (nanoseconds)
 - position: 3d tuple
 - velocity: 3d tuple
+If a robot segment-id is provided, then the meta-data
+is a string repr(robot_position,robot_orientation), with
+robot position a tuple of four floats and orientation a string
+of 4 values (e.g. "0 0 0 0", i.e. mujoco format for orientation).
+Then, for each line:
+ repr([ball_id,time_stamp,position,velocity,robot_time_stamp, joint_positions, joint_velocities])
+with:
+- robot_time_stamp: int (nanoseconds)
+- joint_positions: tuple of 4 values, in radians
+- joint_velocities: tuple of 4 values, in radians per second
 """
 
 
 import sys
+import typing
 import pathlib
 import signal_handler
 import tennicam_client
+import o80_pam
 from lightargs import BrightArgs
 
 
@@ -38,24 +51,52 @@ def _unique_path(
             return path
 
 
-def _run(segment_id: str, filepath: pathlib.Path):
+def _run(
+    segment_id: str, robot_segment_id: typing.Optional[str], filepath: pathlib.Path
+):
     """
     Creates an o80 frontend and uses it to get all
     ball observations and dumping a corresponding
     string in filepath.
+    If robot_segment_id is not None: the current
+    robot's joints position and velocities are added
+    to the observations.
     """
 
     frontend = tennicam_client.FrontEnd(segment_id)
     iteration = frontend.latest().get_iteration()
 
+    robot_frontend: typing.Optional[o80_pam.Frontend]
+    robot_getters: typing.Optional[typing.Tuple[str,...]]
+    meta_data: str
+    
+    if robot_segment_id:
+        robot_frontend = o80_pam.Frontend(robot_segment_id)
+        robot_getters = ("get_positions", "get_velocities")
+        meta_data = repr(pam_mujoco.MujocoRobot.read_positon(robot_segment_id))
+    else:
+        robot_frontend = None
+        robot_getters = None
+        meta_data = ""
+        
     getters = ("get_ball_id", "get_time_stamp", "get_position", "get_velocity")
 
     with filepath.open(mode="w") as f:
+
+        f.write(meta_data+"\n")
+
         try:
             while not signal_handler.has_received_sigint():
                 iteration += 1
                 obs = frontend.read(iteration)
-                values = tuple((getattr(obs, getter)() for getter in getters))
+                if not robot_frontend or not robot_getters:
+                    values = tuple((getattr(obs, getter)() for getter in getters))
+                else:
+                    robot_obs = robot_frontend.latest()
+                    values = tuple(
+                        [getattr(obs, getter)() for getter in getters]
+                        + [getattr(robot_obs, getter)() for getter in robot_getters]
+                    )
                 f.write(repr(values))
                 f.write("\n")
         except (KeyboardInterrupt, SystemExit):
@@ -79,6 +120,12 @@ def _configure() -> BrightArgs:
         str,
     )
     config.add_option(
+        "robot_segment_id",
+        None,
+        "segment_id of the o80 backend of the robot (optional)",
+        str,
+    )
+    config.add_option(
         "filepath",
         str(_unique_path(TENNICAM_CLIENT_DEFAULT_FOLDER)),
         "absolute path of the log file",
@@ -93,5 +140,6 @@ def _configure() -> BrightArgs:
 if __name__ == "__main__":
     config = _configure()
     segment_id = config.segment_id
+    robot_segment_id = config.robot_segment_id
     filepath = pathlib.Path(config.filepath)
-    _run(segment_id, filepath)
+    _run(segment_id, robot_segment_id, filepath)
